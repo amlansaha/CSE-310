e28e8445cf793d8626e9217dad7612111ad6eeac
Amlan Saha <i.amlansaha@gmail.com>
2015-08-29 17:49:51 +0600

    Unnecessary files removed.

--- Assignment1/Debug/Assignment1.d
+++
@@ -1,5 +0,0 @@
-Assignment1.d: ../Assignment1.cpp ../SymbolTable.h ../LinkedList.h
-
-../SymbolTable.h:
-
-../LinkedList.h:
--- Assignment3/script.sh
+++ Assignment3/script.sh
@@ -1,9 +1,9 @@
-bison -d -y parsea.y
-echo 1
-g++ -w -c -o parsea.o y.tab.c
-echo 2
-flex lexa.l
-echo 3
+#bison -d -y parsea.y
+#echo 1
+#g++ -w -c -o parsea.o y.tab.c
+#echo 2
+#flex lexa.l
+#echo 3
 g++ -fpermissive -w -c -o test.o lex.yy.c
 echo 4
 g++ -o demoCompiler test.o parsea.o -lfl -ly
